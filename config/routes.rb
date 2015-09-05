ffec2992630dcb58a8793266dbdc05c56a1525b0
Nalipp <nateplipp@gmail.com>
2015-09-05 11:26:17 +0900

    Integrate stripe payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :books
   devise_for :users
-  
+
   root                'pages#home'
   get  'dashboard' => 'pages#dashboard'
+
+  post '/buy/:slug', to: 'transactions#create', as: :buy
+  get '/pickup/:guid', to: 'transactions#pickup', as: :pickup
 end
