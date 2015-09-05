5d6af8f45b22c827cd945885379c53eac681cb6c
Nalipp <nateplipp@gmail.com>
2015-09-05 10:26:49 +0900

    Added stripe and transactions controller

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'cancan'
 gem 'paperclip', '~> 4.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'friendly_id', '~> 5.1.0'
+gem 'stripe', '~> 1.25.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,10 @@ class Book < ActiveRecord::Base
     friendly_id :name, use: :slugged
 
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
+
+  validates_numericality_of :price,
+  greater_than: 49, message: "must be at least 50 cents"
 end
